ba7f9f2c2cd114d6d1c5eb7e6a446bfca1a723d1
jesse butryn <jesse.butryn@gmail.com>
2026-02-27 12:35:27 -0600

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='bmctools',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
