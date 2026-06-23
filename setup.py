366376b3ad57b1961c900637601c7912a79db0f9
jesse_b <jesse.butryn@gmail.com>
2026-06-23 09:17:03 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='bmctools',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
